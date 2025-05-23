7caab24e7dde15422a0579070de7ac2330dfe310
orinaya <orianefrn@gmail.com>
2025-05-23 09:29:44 +0200

    refactor(archi): change folder structure for build

--- src/services/api.js
+++ src/services/api.js
@@ -1,16 +1,21 @@
-import axios from 'axios'
-require('dotenv').config()
+// import axios from 'axios'
+// require('dotenv').config()
 
-const api = axios.create({
-  baseURL: Config.API_URL,
-  headers: {
-    'Content-Type': 'application/json',
-    Accept: 'application/json'
-  },
-  timeout: 10000
-})
+// const api = axios.create({
+//   baseURL: Config.API_URL,
+//   headers: {
+//     'Content-Type': 'application/json',
+//     Accept: 'application/json'
+//   },
+//   timeout: 10000
+// })
+
+// const hello = {
+  
+// }
+
+// export {
+
+// }
 
 
-export {
-
-}
