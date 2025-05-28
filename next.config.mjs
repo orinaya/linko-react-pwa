57665ef9cf28d19892947c6fb609a9f701d4c769
orinaya <orianefrn@gmail.com>
2025-05-28 14:34:22 +0200

    feat(pwa): add manifest and config pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development', 
+});
+
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+};
+
+// export default nextConfig;
+
+
+module.exports = withPWA(nextConfig);
